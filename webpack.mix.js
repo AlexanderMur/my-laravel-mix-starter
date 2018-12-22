9032535d4dff7e4437098f8f1350e6e365712d2e
Alexander <alexmur99@hotmail.com>
2018-12-23 02:41:00 +0500

    first commit

---
+++ webpack.mix.js
@@ -0,0 +1,115 @@
+/*global Mix*/
+const path = require('path')
+const mix = require('laravel-mix')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const isHMR = process.env.npm_lifecycle_event === 'hot'
+
+// JS files compiling using laravel-mix and react babel presets
+mix.react('src/index.js', 'dist')
+// .react('resources/assets/js/frontend/main.js', 'public/frontend')
+    .setPublicPath('dist')
+/** With this we can extract sass code imported in react components **/
+const rulesConfig = () => {
+    const rulesArray = []
+
+    // ExtractTextPlugin breaks css HMR
+    // Default behaviour is configured for vue in node_modules
+    if (!isHMR) {
+        rulesArray.push({
+            test: /\.s[ac]ss$/,
+            exclude: [], // Fix for newer versions because of webpack-merge usage behind the scenes
+            loader: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                    },
+                ],
+            }),
+        })
+    }
+
+    return rulesArray
+}
+
+// SASS/SCSS webpack loader rules
+// Configure dev server and HMR
+mix.webpackConfig({
+    module: {
+        rules: rulesConfig(),
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: '[name].css',
+            allChunks: true,
+        }),
+    ],
+    devServer: {
+        hot: true, // this enables hot reload
+        inline: true, // use inline method for hmr
+        port: 8080,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+        contentBase: path.resolve(__dirname, 'public'),
+        watchOptions: {
+            exclude: [
+                /bower_components/,
+                /node_modules/,
+            ],
+        },
+    },
+    node: {
+        fs: 'empty',
+        module: 'empty',
+    },
+    // devtool: 'cheap-module-eval-source-map',
+})
+    .sourceMaps()
+    // Comment this line if you want be notified on every change
+    .disableNotifications()
+
+// Apply HMR fix
+// https://github.com/JeffreyWay/laravel-mix/issues/1483
+Mix.listen('configReady', (webpackConfig) => {
+    if (Mix.isUsing('hmr')) {
+        console.log('aaa')
+        // Remove leading '/' from entry keys
+        webpackConfig.entry = Object.keys(webpackConfig.entry).reduce((entries, entry) => {
+            entries[entry.replace(/^\//, '')] = webpackConfig.entry[entry]
+            return entries
+        }, {})
+
+        // Remove leading '/' from ExtractTextPlugin instances
+        webpackConfig.plugins.forEach((plugin) => {
+            if (plugin.constructor.name === 'ExtractTextPlugin') {
+                plugin.filename = plugin.filename.replace(/^\//, '')
+            }
+        })
+    }
+})
+
+// Versioning/Cache Busting
+// Version does not work in hmr mode
+if (!isHMR) {
+    mix.version()
+}
+mix.browserSync({
+    proxy: '',
+    files: [
+        'dist/*.css',
+    ],
+    server: {
+        baseDir: './',
+    },
+})
+// Don't process image files in sass
+// Images paths are written relative to compiled CSS
+mix.options({
+    extractVueStyles: false,
+    processCssUrls: false,
+})
